aed419a4f3334fa253421c7524895e9d8491bd4b
ximada <witch_ada@hotmail.com>
2018-01-08 00:36:54 -0600

    Subiendo archivos

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+/*var menuB =  document.getElementById("menu-bar");
+ 
+ function showMenu(){
+ 	
+ 	var menu = document.getElementById("opcs-menu");
+    if(menu.classList.contains("disabled-menu")){
+
+ 		menu.classList.remove("disabled-menu");
+ 		menu.classList.add("enabled-menu");
+ 	}
+ 	else{
+ 		menu.classList.remove("enabled-menu");
+ 		menu.classList.add("disabled-menu");
+
+ 	}
+ }
+ menuB.addEventListener("click", showMenu);*/
+
+ 
